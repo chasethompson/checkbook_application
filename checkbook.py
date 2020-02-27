8c7ba8c41532974d7838a4fa31f236f514966308
Chase Thompson <chase.w.thompson210@gmail.com>
2020-02-26 19:55:57 -0600

    Commit functioning file creatation and first user selection

--- checkbook.py
+++ checkbook.py
@@ -1,46 +1,54 @@
-import os
+import json
+from datetime import datetime
+import os.path
 import checkbook_functions as cf
 
-transactions = "checkbook_tranactions.txt"
+# Welcome statement for users
+print("\n ---- Welcome to your checkbook ---- ")
 
-# Maybe a checkbook initiation function? To create the checkbook log file or see if it exists and use current?
+# Repeats the loop until user exits program
+while True: 
+    # User selection options
+    print("""
+Please make a selection:
+    1) View Current Balance
+    2) Make A Withdraw
+    3) Make A Deposit
+    4) View Transactions
+    6) Exit (You can also type exit at any prompt to Exit )
+    """) # Not sure yet if how I'll do 4
+    
+    # Requires user to enter valid selection
+    while True: 
+        selection = input("What would you like to do today? ")
+        if selection == "exit":
+            cf.exit_program()
+        elif selection in "1234":
+            break
+        else:
+            print("Invalid entry.")
+    # Open current user checkbook file if it exists, if not creates new file
+    if os.path.exists("checkbook.txt"): 
+        with open("checkbook.txt") as f:
+            checkbook = json.load(f)
+    else: 
+        f= open("checkbook.txt", "w")
+        f.write("[]")
+        with open("checkbook.txt") as f:
+            checkbook = json.load(f)
 
-# First line user sees, welcoming them to the checkbook application
-greeting = "--- Welcome to your terminal checkbook! ---\n"
-print(greeting)
+    # Open current user balance file if it exists, if not creates new file
+    if os.path.exists("balance.txt"):
+        balance = (open("balance.txt", "r"))
+    else: 
+        f= open("balance.txt","w")
+        f.write("0")
+        balance = (open("balance.txt", "r"))
+ 
+    # Print users current balance
+    if selection == "1":
+        print(f"Your balance is ${balance.read()}.")
 
-def menu_prompt():
-    print("What would you like to do? \n")
-    print("1) View your current balance")
-    print("2) Make a withdrawal")
-    print("3) Make a deposit")
-    print("4) Exit")
-    selection = input("Your choice? ")
-    while selection not in "1234":
-        print("Please make a valid selction: " + selection)
-        selection = input("What's would you like to do?")
-    return selection
-
-menu_prompt()
-
-def menu_option(selection):
-    if selection == 1:
-        print(f"Your current balance is {cf.balanace}.\n")
-        return menu_prompt()
-    elif selection == 2:
-        amount = input(f"How much would you like to withdraw?\n")
-        send amount to cf.withdraw function
-    elif selection == 3:
-        amount = input(f"How much would you like to deposit?\n")
-        send amount to cf.deposit function
-    else:
-        print(f"Thank you! Have a good day!")
-
-
-# menu
-# valid
-# invalid
-# balance
-# credit
-# debit
-# exit
+    # Run loop for user withdraw options
+    elif selection == "2":
+        while True: 
